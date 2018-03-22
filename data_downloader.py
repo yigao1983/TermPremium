c700a481da098bc76327d30224e5d7ecb6439163
Yi Gao <yigao1983@gmail.com>
2018-03-22 16:36:39 -0700

    Add data downloader file

---
+++ data_downloader.py
@@ -0,0 +1,33 @@
+import datetime as dt
+import pandas as pd
+
+
+def fetch_yld_data(tenor_lst, start_date, end_date, save=True):
+
+    yld_lst = []
+
+    try:
+        from analytics.emp_analytics.USTreasuryYieldCurveModels import FedReserveNelsonSiegelSvenssonCurve
+        for tenor in tenor_lst:
+            yld_series = FedReserveNelsonSiegelSvenssonCurve.yield_curve(tenor).values(start_date, end_date)
+            yld_series.name = str(int(12*tenor)) + 'M' if tenor < 1 else str(int(tenor)) + 'Y'
+            yld_lst.append(yld_series)
+
+        yld_df = pd.DataFrame(pd.concat(yld_lst, axis=1))
+
+        if save:
+            output_file = 'fed_nss_yld_{}_{}.csv'.format(start_date, end_date)
+            yld_df.to_csv(output_file)
+    except Exception:
+        yld_df = pd.read_csv('fed_nss_yld_{}_{}.csv'.format(start_date, end_date), index_col='Date')
+
+    return yld_df
+
+
+if __name__ == '__main__':
+
+    tenor_lst = [0.25, 0.5, 1, 2, 4, 7, 10]
+    start_date = dt.date(1990, 1, 1)
+    end_date = dt.date(2003, 12, 31)
+
+    fetch_yld_data(tenor_lst, start_date, end_date, True)
